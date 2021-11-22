bef917288dcdbe782f43608d07ce331b76cbe215
Michael Frolov <moyshedev@gmail.com>
2021-11-22 22:53:57 +0200

    Added a gender field to the persona model

--- src/address_book/admin.py
+++ src/address_book/admin.py
@@ -5,13 +5,13 @@ from .models import Person, Phone
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name")
-    list_filter = ("first_name", "last_name")
+    list_display = ("id", "first_name", "last_name", "gender")
+    list_filter = ("first_name", "last_name", "gender")
     search_fields = ("first_name__startswith", "last_name__startswith", )
 
 
 @admin.register(Phone)
 class PhoneAdmin(admin.ModelAdmin):
-    list_display = ("id", "phone", "person")
-    list_filter = ("phone",)
+    list_display = ("phone", "person")
+    list_filter = ("person",)
     search_fields = ("phone__startswith",)
--- src/address_book/models.py
+++ src/address_book/models.py
@@ -5,6 +5,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Person(models.Model):
     first_name = models.CharField("Name", max_length=200)
     last_name = models.CharField("Surname", max_length=200)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(null=False, max_length=1, choices=GENDER_CHOICES, default=None)
+
+    class Meta:
+        unique_together = ('first_name', 'last_name',)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
